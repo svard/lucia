b0b6dfcabb7846753386cb616cc9877af1841012
Kristofer Svärd <kristofer.svard@gmail.com>
2015-09-22 19:51:40 +0200

    Cleaning up

--- lib/lucia/controller.ex
+++ lib/lucia/controller.ex
@@ -1,22 +1,11 @@
 defmodule Lucia.Controller do
   require Logger
   use Timex
-  alias Lucia.State
   alias Lucia.Fsm
 
   def check(%{date: date, level: level}) when level < 0.1 do
     {:ok, dt} = Timex.Parse.DateTime.Parser.parse(date, "{ISOz}")
     Logger.debug "Level #{level}"
-
-    # if !State.get.triggered do
-    #   Logger.debug "Level #{level}, incrementing threshold"
-    #   State.increment()
-    # end
-
-    # if State.get.threshold > 3 && !State.get.triggered && dt.hour < 23 do
-    #   Logger.debug "Threshold #{State.get.threshold}, trigger lights"
-    #   State.trigger()
-    # end
 
     if dt.hour < 23 do
       Fsm.next
@@ -25,14 +14,6 @@ defmodule Lucia.Controller do
 
   def check(%{date: date, level: level}) do
     Logger.debug "Level #{level}"
-    # {:ok, dt} = Timex.Parse.DateTime.Parser.parse(date, "{ISOz}")
-
-    # if dt.hour == 12 && State.get.triggered do
-    #   Logger.debug "Resetting state"
-    #   State.reset()
-    # end
-
-    # State.reset_threshold()
 
     Fsm.reset
   end  
